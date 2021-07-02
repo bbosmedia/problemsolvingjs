a7d868c5829e4ecf637d8ab9e66409c5978a8bd8
bbosmedia <bbosmedia@users.noreply.github.com>
2021-07-02 22:13:56 +0500

    Function Helps us to determine number is odd or even

---
+++ problem21.js
@@ -0,0 +1,14 @@
+function checkNumber(num){
+    if(num % 2 === 0){
+        console.log(`${num} is Even`)
+    }else{
+        console.log(`${num} is Odd`)
+    }
+}
+
+checkNumber(4)
+checkNumber(5)
+checkNumber(17)
+checkNumber(1888)
+checkNumber(3743)
+checkNumber(4323)
